d6175a9a26e0c9e848d59e219d29664e5e0291dc
wrxue <a0981355215@gmail.com>
2020-09-09 19:28:20 +0800

    init

---
+++ CorePractice/Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace CorePractice.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string[] names = new string[] { "Alfreda Stephenson",
+                  "Swanson Swanson",
+                  "Walker Pace",
+                  "Katina Le",
+                  "Lesley Calderon",
+                  "Ruth Merrill",
+                  "Cristina Buchanan",
+                  "Gallegos May",
+                  "Macdonald Lucas"};
+            int num = 3;
+            int[] age = new int[num];
+            string[] name = new string[num];
+            int[] family = new int[num];
+            Random rnd = new Random();
+            for (int i = 0; i < num; i++)
+            {
+                age[i] = rnd.Next(50);
+                name[i] = names[rnd.Next(names.Length)];
+                family[i] = rnd.Next(5);
+            }
+            ViewData["age"] = age;
+            ViewData["name"] = name;
+            ViewData["family"] = family;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# This project is a very simple example of ASP.NET Core web application.
+### Enveironment: Visual Studio Enterprise 2019, Windows10 x64.
