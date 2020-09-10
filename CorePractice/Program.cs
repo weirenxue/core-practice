8c4561c16c1117f38810ee5c382adea2b93d2d0a
wrxue <a0981355215@gmail.com>
2020-09-10 15:26:37 +0800

    改為不只有本機(localhost)可以連線

--- CorePractice/Program.cs
+++ CorePractice/Program.cs
@@ -14,13 +14,14 @@ namespace CorePractice
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:5000").UseStartup<Startup>();
                 });
     }
 }
